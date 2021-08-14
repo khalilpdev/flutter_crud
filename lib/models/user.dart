0f81500b01241bfee2aff24abf18e87a156ff932
leandro khalil <khalilpdev@gmail.com>
2021-08-14 11:43:04 -0300

    criado metodos adicionais de map e json no model de usuario

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class User {
@@ -12,4 +14,26 @@ class User {
     @required this.email,
     @required this.avatarUrl,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'avatarUrl': avatarUrl,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      avatarUrl: map['avatarUrl'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
 }
